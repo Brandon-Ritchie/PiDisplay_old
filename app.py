f6b6c6d474c46514232345fcaae07032d4d7c53a
Brandon Ritchie <britchie@pop-os.localdomain>
2021-09-09 12:12:42 -0400

    added authentication and moved models to seperate file

--- app.py
+++ app.py
@@ -3,16 +3,13 @@ Imports
 """
 
 from logging import error
-from flask import Flask, flash, render_template, request
+from flask import Flask, flash, render_template, request, redirect
 from flask.helpers import url_for
-from werkzeug.datastructures import RequestCacheControl
-from werkzeug.utils import redirect
-from flask_login import LoginManager, login_required, login_user, UserMixin
-from flask_wtf import form
+from flask_login import LoginManager, login_required, login_user
 from forms import CrontabForm, LoginForm, update_crontab_form_defaults, return_list_of_entries_as_lists, return_form_data_as_list_of_dict, update_display_entry # for form manipulation
 from flask_sqlalchemy import SQLAlchemy # for database manipulation
 from utilities import update_crontab # for updating crotab
-from werkzeug.security import generate_password_hash, check_password_hash
+
 
 """
 Database and app configuration
@@ -26,29 +23,7 @@ db = SQLAlchemy(app) # database instance
 app_login_manager = LoginManager(app)
 app_login_manager.init_app(app)
 
-class DisplayEntry(db.Model):
-    id = db.Column(db.Integer, primary_key = True) # primary key column, automatically generated IDs
-    day_of_the_week = db.Column(db.String(15), index = True, unique = True) # Day of the week for the entry
-    start_time = db.Column(db.String(10), index = True, unique = False) # Time for display to turn on
-    switch_time = db.Column(db.String(10), index = True, unique = False) # Time for display to switch slides
-    end_time = db.Column(db.String(10), index = True, unique = False) # time for display to turn off
-    start_link_text = db.Column(db.String(30), index = True, unique = False) # Display link text for first slide
-    switch_link_text = db.Column(db.String(30), index = True, unique = False) # Display link text for second slide
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password) 
+import models
 
 """
 Routes
@@ -56,7 +31,7 @@ Routes
 
 @app_login_manager.user_loader
 def load_user(id):
-  return User.query.get(int(id))
+  return models.User.query.get(int(id))
 
 @app_login_manager.unauthorized_handler
 def unauthorized():
@@ -66,7 +41,7 @@ def unauthorized():
 def index():
     login_form = LoginForm()
     if request.method == 'POST':
-        user = User.query.filter_by(username=login_form.user.data).first()
+        user = models.User.query.filter_by(username=login_form.user.data).first()
         if user:
             if user.check_password(login_form.password.data):
                 login_user(user)
@@ -86,7 +61,7 @@ def home():
             entry_list = return_form_data_as_list_of_dict(crontab_form) # created as dictionaries of the form data making it easier to deal with
 
             for entry in entry_list:
-                update_display_entry(DisplayEntry, entry) # update each entry in the DisplayEntry table with the entries from created dictionaries
+                update_display_entry(models.DisplayEntry, entry) # update each entry in the DisplayEntry table with the entries from created dictionaries
             try:
                 db.session.commit()
                 flash('The display has been updated.')
@@ -94,12 +69,12 @@ def home():
                 db.session.rollback()
                 flash('There was an error')
 
-            update_crontab(DisplayEntry)
+            update_crontab(models.DisplayEntry)
 
         if crontab_form.cancel.data:
             db.session.rollback()
 
-    some_list = return_list_of_entries_as_lists(DisplayEntry) # create list for updating crontab form defaults
+    some_list = return_list_of_entries_as_lists(models.DisplayEntry) # create list for updating crontab form defaults
     update_crontab_form_defaults(crontab_form, some_list) # update crontab form defaults AFTER the POST request
     
     return render_template('home.html',
--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ sudo pip3 install Flask-WTF &&
 sudo pip3 install Flask-SQLAlchemy &&
 sudo pip3 install python-crontab &&
 sudo pip3 install flask-login &&
+sudo pip3 install werkzeug &&
 pip3 install selenium &&
 pip3 install PyAutoGui &&
 sudo apt install cec-utils -y &&

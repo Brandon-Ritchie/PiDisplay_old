7ac7fad217d586e1d88cfa77f8d64abc2a55a527
LaTromba <ritchietrumpet@gmail.com>
2021-09-30 11:07:37 -0400

    Updated install to renamed git repo

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt upgrade -y &&
 
 # Make Scripts Directory and copy scripts
 mkdir /home/pi/Scripts &&
-git clone https://github.com/LaTromba/PiDisplay.git /home/pi/Scripts/PiDisplay &&
+git clone https://github.com/Brandon-Ritchie/PiDisplay.git /home/pi/Scripts/PiDisplay &&
 
 # Install dependencies
 sudo pip3 install Flask-WTF &&

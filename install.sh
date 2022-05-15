044a08d5fb8d03a9d38f9069af366dce232201c8
Brandon-Ritchie <britchie@brandonritchie.tech>
2022-05-15 15:11:09 -0400

    Fixed typo in path in install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt update &&
 sudo apt upgrade -y &&
 
 # Install dependencies
-sudo pip3 install -r home/pi/Scripts/PiDisplay/requirements.txt
+sudo pip3 install -r /home/pi/Scripts/PiDisplay/requirements.txt
 sudo apt install cec-utils -y &&
 sudo apt install chromium-chromedriver

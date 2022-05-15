7662ec7b96c5eb00e6c5f85d77b6f47f64a0af00
Brandon-Ritchie <britchie@brandonritchie.tech>
2022-05-15 14:29:05 -0400

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,7 @@
 sudo apt update &&
 sudo apt upgrade -y &&
 
-# Make Scripts Directory and copy scripts
-mkdir /home/pi/Scripts &&
-git clone https://github.com/Brandon-Ritchie/PiDisplay.git /home/pi/Scripts/PiDisplay &&
-
 # Install dependencies
-sudo pip3 install -r requirements.txt
+sudo pip3 install -r home/pi/Scripts/PiDisplay/requirements.txt
 sudo apt install cec-utils -y &&
 sudo apt install chromium-chromedriver
